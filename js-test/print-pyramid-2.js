185429551255b367c0e063b8099f6dab4d02156e
Paul Rodriguez <paul@prdesign.work>
2018-05-17 15:20:25 -0600

    First Commit

---
+++ js-test/print-pyramid-2.js
@@ -0,0 +1,23 @@
+const space = function(spc) {
+  if (spc === 0) {
+    return "";
+  }
+  return " " + space(spc - 1);
+};
+const star = function(str) {
+  if (str === 0) {
+    return "";
+  }
+  return "*" + star(str - 1);
+};
+const run = function(n, spc, str) {
+  if (n === 0) {
+    return " ";
+  }
+  console.log(space(spc) + star(str));
+  run(n - 1, spc - 1, str + 2);
+};
+const trianglestars = function(n) {
+  run(n, n - 1, 1);
+};
+trianglestars(5);
